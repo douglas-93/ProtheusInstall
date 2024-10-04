88f4135d85a1c11ca9d1cbd6c297da815d799c0f
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2024-10-04 08:54:43 -0300

    Instalação completa

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import os
-import shutil
+import tempfile
+import zipfile
 
+from win32com.client import Dispatch
+
+TEMP_DIR = tempfile.mkdtemp()
 PROTHEUS_DIR = r'\\192.168.100.30\Instalaveis\smartclient 03.06\smartclient.zip'
-DESTINATION_DIR = r'C:\Users\douglas.oliveira\Desktop\smartclient.zip'
+DESTINATION_COPY_FILE = os.path.join(TEMP_DIR, 'smartclient.zip')
+DESTINATION_FOLDER = r'C:\\'
+TARGET_EXE_PATH = r'C:\smartclient\smartclient.exe'
+PUBLIC_DESKTOP_PATH = r'C:\Users\Public\Desktop\Protheus.lnk'
+
 
 def copy_file_with_progress(source: str, destination: str):
+    print("Copiando arquivo do servidor ...")
     # Tamanho total do arquivo
     total_size = os.path.getsize(source)
     copied_size = 0
@@ -21,9 +30,46 @@ def copy_file_with_progress(source: str, destination: str):
             copied_size += len(buffer)
             # Calcula e exibe o progresso
             progress = (copied_size / total_size) * 100
-            print(f'\rProgresso: {progress:.2f}% ({copied_size} de {total_size} bytes)', end='')
+            print(f'\rConcluído: {progress:.2f}% ({copied_size} de {total_size} bytes)', end='')
 
     print("\nCópia concluída!")
 
-# Chamar a função para copiar o arquivo
-copy_file_with_progress(PROTHEUS_DIR, DESTINATION_DIR)
+
+# Função para descompactar com progresso
+def unzip_file_with_progress(zip_file: str, destination_dir: str):
+    print("Iniciando descompactação do arquivo copiado ...")
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        # Obtém a lista de todos os arquivos no .zip
+        total_files = len(zip_ref.infolist())
+        for idx, file in enumerate(zip_ref.infolist(), start=1):
+            zip_ref.extract(file, destination_dir)
+            # Calcula e exibe o progresso
+            progress = (idx / total_files) * 100
+            print(f'\rConcluído: {progress:.2f}% ({idx}/{total_files})', end='')
+
+    print("\nDescompactação concluída!")
+
+
+# Função para criar atalho na área de trabalho pública
+def create_shortcut(target, shortcut_path):
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortcut(shortcut_path)
+
+    # Definir o caminho do executável (programa alvo)
+    shortcut.TargetPath = target
+    shortcut.WorkingDirectory = os.path.dirname(target)
+
+    # Descrição opcional do atalho
+    shortcut.Description = "Protheus"
+
+    # Salva o atalho no local especificado
+    shortcut.Save()
+    print("Atalho criado com sucesso!")
+
+
+copy_file_with_progress(PROTHEUS_DIR, DESTINATION_COPY_FILE)
+unzip_file_with_progress(DESTINATION_COPY_FILE, DESTINATION_FOLDER)
+create_shortcut(TARGET_EXE_PATH, PUBLIC_DESKTOP_PATH)
+print("Limpando arquivos desnecessários ...")
+os.remove(DESTINATION_COPY_FILE)
+print("Instalação concluída!")
